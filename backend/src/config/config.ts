05b522046abf4824bf3031546656b0294b12b58e
Matias-000 <matias.cardoso3001@gmail.com>
2025-08-14 13:56:21 -0300

    frontend url added in backend

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,4 +1,4 @@
 export const PORT = process.env.PORT || 8080
 export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN || 'yoursecret'
 export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN || 'yoursecret'
-
+export const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173'
