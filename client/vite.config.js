71b856d7230f5ab4ae1f4b568c0134ad24c10d10
Matias-000 <matias.cardoso3001@gmail.com>
2025-08-17 19:46:50 -0300

    new build

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import ssr from 'vite-plugin-ssr/plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),ssr()],
+  plugins: [react(), tailwindcss()],
 })
