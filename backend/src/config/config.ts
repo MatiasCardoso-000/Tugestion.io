d654a8e1b152feb8c2a1249d68095b77d787d014
Matias-000 <matias.cardoso3001@gmail.com>
2025-08-18 13:12:54 -0300

    added script start to package client

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,4 +1,5 @@
-export const PORT = process.env.PORT || 8080
-export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN || 'yoursecret'
-export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN || 'yoursecret'
-export const FRONTEND_URL = process.env.FRONTEND_URL || 'https://tugestion-5i6te6rhk-matias-projects-1fee9a15.vercel.app/'
+export const PORT = process.env.PORT || 3312;
+export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN || "yoursecret";
+export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN || "yoursecret";
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+export const DATABASE_URL = process.env.DATABASE_URL || 'yourdatabase'
